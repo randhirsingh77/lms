56cf362a5aefc8a5b55b593fe019f84080fbfc31
randhirsingh77 <rs6201849307@gmail.com>
2025-09-06 20:57:54 +0530

    cors change

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://lms-3bcomgpac-randhir-singhs-projects-1ae44db2.vercel.app/",
     credentials:true
 }));
  
