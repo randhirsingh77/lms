3aca6dab0eb74c74d82310e3469e5062f0a0ae09
randhirsingh77 <rs6201849307@gmail.com>
2025-09-06 21:11:10 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://lms-3bcomgpac-randhir-singhs-projects-1ae44db2.vercel.app/",
+    origin:"https://lms-3bcomgpac-randhir-singhs-projects-1ae44db2.vercel.app",
     credentials:true
 }));
  
